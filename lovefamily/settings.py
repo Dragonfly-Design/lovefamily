7deeb3fe125c13f36359edb7dea75c236d202e0b
Nolan Love <nolanlove@yahoo.com>
2016-11-20 01:10:27 -0800

    Update DB connection to use postgres

--- lovefamily/settings.py
+++ lovefamily/settings.py
@@ -72,13 +72,21 @@ WSGI_APPLICATION = 'lovefamily.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+
+# Database connection
+# Sample: postgres://[user]:[password]@localhost/[dbname]
+DATABASE_URL = os.environ['DATABASE_URL']
+
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 
 # Password validation
