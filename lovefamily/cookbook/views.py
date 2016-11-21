3cab4c005ba86ce832020b5a8eaceb07b8b2000a
Nolan Love <nolanlove@yahoo.com>
2016-11-20 16:38:19 -0800

    Add Foundation

--- lovefamily/cookbook/views.py
+++ lovefamily/cookbook/views.py
@@ -1,36 +1,28 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Document, Page
-from settings import BASE_DIR
-
-
-# Create your views here.
-
-
-from django.http import HttpResponse
+from .models import Page
 
 
 def index(request):
-
-    print 'index'
-    print BASE_DIR
-
     pages = None
     error_message = None
-    search_terms = ''
+    search_terms = None
+
+    print('HELLO')
 
     try:
         search_terms = request.POST['search_terms']
         pages = Page.objects.filter(search_text__icontains=request.POST['search_terms'])
-        if not pages:
-            error_message = "Search returned no results."
-    except (KeyError):
+        if search_terms and not pages:
+            error_message = "'%s' not found." % search_terms
+    except KeyError:
         pass
 
     if not pages:
         pages = Page.objects.all()
-    return render(request, 'cookbook/index.html', {'pages': pages, 'error_message': error_message, 'search_terms': search_terms})
+    return render(request, 'cookbook/index.html',
+                  {'pages': pages, 'error_message': error_message, 'search_terms': search_terms})
+
 
 def page(request, page_id=None):
     page = get_object_or_404(Page, page_number=page_id)
     return render(request, 'cookbook/page.html', {'page': page})
-
